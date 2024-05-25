d90b96bb522e265556de8a99ef7813fe5bb4a602
gregorygustavo80 <gregorygustavo80@gmail.com>
2024-05-25 12:58:06 -0300

    firt commit

---
+++ Mr.Robot.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+df = pd.read_excel('') # Insira o caminho do aquivo aqui
+
+chrome = webdriver.Chrome()
+url = '' #insira sua url aqui
+
+chrome.get(url)
+
+time.sleep(35)
+
+for index, row in df.iterrows():
+    try:
+        notas = chrome.find_element(By.XPATH, '') #insira o XPATH aqui
+        notas.clear()
+        notas.send_keys(row['Nome da coluna'])
+    except Exception as e:
+        print(f"Erro ao encontrar o elemento pelo XPath: {e}")
+        continue
+
+    time.sleep(1)
+
+chrome.quit()
+
+input('Pressione qualquer tecla para sair')
